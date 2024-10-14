e7fc046931e3ca5b1c3eb0c6cd432c816852556c
Csaba Hell <ifj.hellcsaba@gmail.com>
2024-10-14 13:00:32 +0200

    Add form validation logic and error message

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const subscribeForm = document.querySelector(".subscribe__form");
+const emailInput = document.getElementById("email");
+const errorMessage = document.querySelector(".subscribe__form-error-message");
 
 subscribeForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const subscribeImageContainer = document.querySelector(
-    ".subscribe__image-container"
-  );
-  const subscribeFormContainer = document.querySelector(
-    ".subscribe__form-container"
-  );
+  const emailValue = emailInput.value.trim();
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  subscribeFormContainer.style.display = "none";
-  subscribeImageContainer.style.display = "none";
+  if (!emailPattern.test(emailValue)) {
+    emailInput.classList.add("subscribe__form-input--error");
+    errorMessage.style.display = "block";
+  } else {
+    const subscribeSuccess = document.querySelector(".subscribe__success");
+    subscribeSuccess.classList.remove("subscribe__success--hidden");
+    emailInput.classList.remove("subscribe__form-input--error");
+    errorMessage.style.display = "none";
 
-  const subscribeSuccess = document.querySelector(".subscribe__success");
-
-  subscribeSuccess.classList.remove("subscribe__success--hidden");
+    const subscribeImageContainer = document.querySelector(
+      ".subscribe__image-container"
+    );
+    const subscribeFormContainer = document.querySelector(
+      ".subscribe__form-container"
+    );
+    subscribeFormContainer.style.display = "none";
+    subscribeImageContainer.style.display = "none";
+  }
 });
